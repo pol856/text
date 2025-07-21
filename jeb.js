4838bef0fafcf4cbf96a15e7e72a548f5c221f79
pol856 <67319010013@thonburi.ac.th>
2025-07-21 14:30:00 +0700

    Paphop

---
+++ jeb.js
@@ -0,0 +1,22 @@
+const display = document.getElementById('display');
+
+function append(char) {
+  if (display.innerText === '0' || display.innerText === 'Error') {
+    display.innerText = char;
+  } else {
+    display.innerText += char;
+  }
+}
+
+function clearDisplay() {
+  display.innerText = '0';
+}
+
+function calculate() {
+  try {
+    let result = eval(display.innerText.replace('%', '/100'));
+    display.innerText = result;
+  } catch (e) {
+    display.innerText = 'Error';
+  }
+}
